03f7bb8537394df010309ed5441aacce055d068c
Toqui012 <alvaroseba2000@gmail.com>
2021-10-05 22:12:09 -0300

    Incorporación listado de emepleados

--- DashboardApp/urls.py
+++ DashboardApp/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from DashboardApp.views import DashboardMain
+from DashboardApp.views import UserSection
 from LoginApp.views import logout
 
 urlpatterns = [
     path('', DashboardMain, name='DashboardMain'),
+    path('userSection/', UserSection, name='UserSection'),
     path('logout/', logout, name='logout'),
 ]
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -3,11 +3,37 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template.context_processors import request
 from LoginApp.views import authenticated, decodered
+import requests
 
 # Create your views here.
 
 def DashboardMain(request):
     if authenticated(request):
-        return render(request, 'dashboard.html')
+
+        #Variables con data a enviar a la vista
+        context = {
+        }
+
+        # Return Section
+        return render(request, 'dashboard.html', {'data': context})
+    else:
+        return redirect('login')
+
+def UserSection(request):
+    if authenticated(request):
+        # Consumo de API: Usuario
+        # Method: GET
+        token = request.COOKIES.get('validate')
+        headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
+        req = requests.get('http://localhost:32482/api/usuario', headers=headers)
+        dataAPI = req.json()
+        listUser = dataAPI['data']
+
+        #Variables con data a enviar a la vista
+        context = {
+            'users': listUser
+        }
+        # Return Section
+        return render(request, 'list_user.html', {'data': context})
     else:
         return redirect('login')
