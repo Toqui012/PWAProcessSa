d7b56885546cab8b1a219157b63306dd102d0978
Toqui012 <alvaroseba2000@gmail.com>
2021-10-22 01:34:06 -0300

    Asignar Tareas Finalizado

--- AsignarTareasApp/urls.py
+++ AsignarTareasApp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
-from AsignarTareasApp.views import AsignarTareaSection, AsignarTareaUsuarioSection
+from AsignarTareasApp.views import AsignarTareaSection, AsignarTareaUsuarioSection, AssignTask
 from LoginApp.views import logout
 
 urlpatterns = [
     path('asignarTareaSection/', AsignarTareaSection, name="AsignarTareaSection"),
-    path('asignarTareaUsuarioSection/', AsignarTareaUsuarioSection, name="AsignarTareaUsuarioSection"), 
+    path('asignarTareaUsuarioSection/<int:idTask>', AsignarTareaUsuarioSection, name="AsignarTareaUsuarioSection"),
+    path('assignTaskProcedure/<str:idTask>/<str:rutUser>', AssignTask, name="AssignTaskProcedure"), 
     path('logout/', logout, name='logout'),
 ]
--- AsignarTareasApp/views.py
+++ AsignarTareasApp/views.py
@@ -10,23 +10,57 @@ import requests, jwt, json
 
 # Create your views here.
 
+# Función encargada de mostrar el listado de tareas disponibles a asignar
 def AsignarTareaSection(request):
-    print("Hello world :D")
-    return render(request, 'Asignar_Tarea/asignar_tarea.html')
-
-def AsignarTareaUsuarioSection(request):
     if authenticated(request):
+        token = request.COOKIES.get('validate')
+        headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
+        reqTask = requests.get('http://localhost:32482/api/tarea/', headers=headers)
+        dataAPI = reqTask.json()
+        listTask = dataAPI['data']
+
+        context = {
+            'task': listTask
+        }
+
         # Return Section
+        return render(request, 'Asignar_Tarea/asignar_tarea.html',{'data': context})
+    else:
+        redirect('login')
+
+# Función encargada de asignar una tarea a un usuario
+def AsignarTareaUsuarioSection(request, idTask):
+    if authenticated(request):
         token = request.COOKIES.get('validate')
         headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
         reqUsers = requests.get('http://localhost:32482/api/usuario', headers=headers)
         dataAPI = reqUsers.json()
         listUsers = dataAPI['data']
-
         # Variables con data a enviar a la vista
         context = {
-            'users': listUsers
+            'users': listUsers,
+            'idTask': idTask
         }
 
+        # Return Section
         return render(request, 'Asignar_Tarea/list_user_asignar.html', {'data': context})
-    
+    else:
+        redirect('login')
+
+def AssignTask(request, idTask, rutUser):
+    if authenticated(request):
+        try:
+            token = request.COOKIES.get('validate')
+            headers = {'Accept-Encoding': 'UTF-8', 'Content-Type': 'application/json', 'Authorization': 'Bearer '+ token,'Accept': '*/*' }
+            reqAssignTask = requests.put('http://localhost:32482/api/tarea/assignTask/'+ idTask + '/' + rutUser , headers=headers)
+            print(reqAssignTask)
+            return redirect('AsignarTareaSection')
+        except:
+            print('no funciona')
+
+    else:
+        redirect('login')
+        
+
+
+
