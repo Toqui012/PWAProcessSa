6c9c62aeb8797e6d7b581dd03237f7b02ca40c89
a.berriosh <a.berriosh@alumnos.duoc.cl>
2021-11-24 22:56:20 -0300

    avance_informes

--- PdfGenerator/urls.py
+++ PdfGenerator/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
-from PdfGenerator.views import TestPDF
+from PdfGenerator.views import TestPDF, GrafictSection
 
 urlpatterns = [
-    path('pdf/',TestPDF.as_view(), name='pdf_generator'),
+    path('pdf/1',TestPDF.as_view(), name='pdf_generator'),
+    path('pdf/2',GrafictSection, name='pdf_generator2'),
 ]
--- PdfGenerator/views.py
+++ PdfGenerator/views.py
@@ -15,6 +15,87 @@ from xhtml2pdf import pisa
 from django.views.generic import TemplateView, View
 # Create your views here.
 
+def GrafictSection(request):
+    if authenticated(request):
+        token = request.COOKIES.get('validate')
+        headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
+        
+        
+        if request.method == 'POST':
+            
+            option = request.POST.get('grapichSelection')
+            if option == 'tareas':
+                return graphicTarea(request)
+            if option == 'usuario':
+                return graphicUsuario(request)
+            else:
+                return HttpResponseRedirect(reverse_lazy('DashboardMain'))
+        return render(request,'graphicSelection.html')
+
+
+def graphicTarea(request):
+    if authenticated(request):
+        
+        template = get_template('Template_informe/tareas.html')
+        # Consumo de API: Usuario
+        # Method: GET
+        token = request.COOKIES.get('validate')
+        headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
+        req = requests.get('http://localhost:32482/api/tarea', headers=headers)
+        dataAPI = req.json()
+        listTarea = dataAPI['data']
+        
+        #Variables con data a enviar a la vista
+        context = {
+            'tareas': listTarea,
+            'name':'Tareas'
+        }
+        # Return Section
+        html = template.render(context)
+        response = HttpResponse(content_type='application/pdf')
+        #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+        pisaStatus = pisa.CreatePDF(
+            html, dest=response
+        )
+        print(response)
+        if pisaStatus.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
+    else:
+        return HttpResponseRedirect(reverse_lazy('DashboardMain'))
+
+
+def graphicUsuario(request):
+    if authenticated(request):
+        
+        template = get_template('invoice.html')
+        # Consumo de API: Usuario
+        # Method: GET
+        token = request.COOKIES.get('validate')
+        headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
+        req = requests.get('http://localhost:32482/api/usuario', headers=headers)
+        dataAPI = req.json()
+        listTarea = dataAPI['data']
+        
+        #Variables con data a enviar a la vista
+        context = {
+            'tareas': listTarea,
+            'name':'Usuario'
+        }
+        # Return Section
+        html = template.render(context)
+        response = HttpResponse(content_type='application/pdf')
+        #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+        pisaStatus = pisa.CreatePDF(
+            html, dest=response
+        )
+        print(response)
+        if pisaStatus.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
+    else:
+        return HttpResponseRedirect(reverse_lazy('DashboardMain'))
+
 
 
 class TestPDF(TemplateView):
@@ -28,7 +109,6 @@ class TestPDF(TemplateView):
             req = requests.get('http://localhost:32482/api/tarea', headers=headers)
             dataAPI = req.json()
             listTarea = dataAPI['data']
-
             #Variables con data a enviar a la vista
             context = {
                 'tareas': listTarea
@@ -42,47 +122,10 @@ class TestPDF(TemplateView):
             )
             if pisaStatus.err:
                 return HttpResponse('We had some errors <pre>' + html + '</pre>')
+            print(response)
             return response
         else:
             return HttpResponseRedirect(reverse_lazy('DashboardMain'))
-
-
-
-
-
-
-
-        # try:
-        #     #obtengo el template que va hacer de base para hacer el pdf
-        #     template = get_template('invoice.html')
-        #     #creo un diccionario context con las vriable que voy a usar y lo envio con render
-        #     print('okey')
-        #     token = request.COOKIES.get('validate')
-        #     headers = {'Content-Type':'application/json', 'Authorization': 'Bearer '+ token}
-        #     req = requests.get('http://localhost:32482/api/tarea', headers=headers)
-        #     dataAPI = req.json()
-        #     listTarea = dataAPI['data']
-            
-        #     context = {
-        #         'tareas':listTarea,
-        #         'tittle':'hola'
-        #     }
-        #     html = template.render(context)
-            
-        #     #se crea el pdf y se descarga
-        #     response = HttpResponse(content_type='application/pdf')
-        #     #response['Content-Disposition'] = 'attachment; filename="report.pdf"' # esto hace que el pdf se descargue directamente
-
-        #     # llama a la libreria xhtml2pdf para crear el pdf
-        #     pisaStatus = pisa.CreatePDF(
-        #         html, dest=response
-        #     )
-        #     if pisaStatus.err:
-        #         return HttpResponse('We had some errors <pre>' + html + '</pre>')
-        #     return response
-        # except:
-        #     pass
-        # return HttpResponseRedirect(reverse_lazy('DashboardMain'))
 
 
 
