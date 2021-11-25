71850cbfcbed9eb36a1a4aeef40ba15909eeb54e
a.berriosh <a.berriosh@alumnos.duoc.cl>
2021-11-25 17:57:56 -0300

    arreglada_plantilla_pdf

--- PdfGenerator/urls.py
+++ PdfGenerator/urls.py
@@ -7,16 +7,5 @@ from PdfGenerator.views import TestPDF, GrafictSection
 from PdfGenerator.views import PDFSection, ReporteEmpleados, ReporteEmpresas, ReporteTareaSubordinada, TestPDF
 
 urlpatterns = [
-    path('pdfSection/', PDFSection, name='PDFSection'),
-    # PDF Tarea
-    path('pdfTarea/',TestPDF.as_view(), name='pdf_generator'),
-    # PDF Tarea Subordinada
-    #path('pdfTareaSubordinada/', ReporteTareaSubordinada.as_view(), name='ReporteTareaSubordinada'),
-    # PDF Empresas
-    #path('pdfEmpresa/', ReporteEmpresas.as_view(), name='ReporteEmpresa'),
-    # PDF Empleados
-    #path('pdfEmpleado/', ReporteEmpleados.as_view(), name='ReporteEmpleado'),
-
-    path('pdf/1',TestPDF.as_view(), name='pdf_generator'),
-    path('pdf/2',GrafictSection, name='pdf_generator2'),
+    path('pdf/',GrafictSection, name='pdf_generator'),
 ]
